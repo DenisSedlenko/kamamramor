55b5bccae93ac1a1c0826be709b009b09de353cb
Denis Sedlenko <v-dealek@microsoft.com>
2018-07-26 09:46:01 +0300

    fix js script

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,35 +7,35 @@ $(document).on('ready', function() {
   function slideInitial() {
     $('.material-single').addClass('proactivede');
     setTimeout(function() {
-      slideRight($('.material-single'), -1, $('.material-single').length-1);
+      slideRight({ slide:$('.material-single'), current: -1, total: $('.material-single').length-1 });
     }, 500);
     $('.o-work-single').addClass('proactivede');
     setTimeout(function() {
-      slideRight($('.o-work-single'), -1, $('.o-work-single').length-1);
+      slideRight({ slide: $('.o-work-single'), current: -1, total: $('.o-work-single').length-1 });
     }, 500);
   }
 
-  function slideRight(sl, cur, total) {
-    if (cur < total) {
-      cur++;
+  function slideRight(obj) {
+    if (obj.current < obj.total) {
+      obj.current++;
     } else {
-      cur = 0;
+      obj.current = 0;
     }
 
-    if (cur > 0) {
-      var preactiveSlide = sl.eq(cur - 1);
+    if (obj.current > 0) {
+      var preactiveSlide = obj.slide.eq(obj.current - 1);
     } else {
-      var preactiveSlide = sl.eq(total);
+      var preactiveSlide = obj.slide.eq(obj.total);
     }
-    var activeSlide = sl.eq(cur);
-    if (cur < total) {
-      var proactiveSlide = sl.eq(cur + 1);
+    var activeSlide = obj.slide.eq(obj.current);
+    if (obj.current < obj.total) {
+      var proactiveSlide = obj.slide.eq(obj.current + 1);
     } else {
-      var proactiveSlide = sl.eq(0);
+      var proactiveSlide = obj.slide.eq(0);
 
     }
 
-    sl.each(function() {
+    obj.slide.each(function() {
       var thisSlide = $(this);
       if (thisSlide.hasClass('preactivede')) {
         thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
@@ -49,25 +49,25 @@ $(document).on('ready', function() {
     proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
   }
 
-  function slideLeft() {
-    if (slideCurrent > 0) {
-      slideCurrent--;
+  function slideLeft(obj) {
+    if (obj.current > 0) {
+      obj.current--;
     } else {
-      slideCurrent = slideTotal;
+      obj.current = obj.total;
     }
 
-    if (slideCurrent < slideTotal) {
-      var proactiveSlide = slide.eq(slideCurrent + 1);
+    if (obj.current < obj.total) {
+      var proactiveSlide = obj.slide.eq(obj.current + 1);
     } else {
-      var proactiveSlide = slide.eq(0);
+      var proactiveSlide = obj.slide.eq(0);
     }
-    var activeSlide = slide.eq(slideCurrent);
-    if (slideCurrent > 0) {
-      var preactiveSlide = slide.eq(slideCurrent - 1);
+    var activeSlide = obj.slide.eq(obj.current);
+    if (obj.current > 0) {
+      var preactiveSlide = obj.slide.eq(obj.current - 1);
     } else {
-      var preactiveSlide = slide.eq(slideTotal);
+      var preactiveSlide = obj.slide.eq(obj.total);
     }
-    slide.each(function() {
+    obj.slide.each(function() {
       var thisSlide = $(this);
       if (thisSlide.hasClass('proactivede')) {
         thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
@@ -81,36 +81,31 @@ $(document).on('ready', function() {
     proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
   }
 
-  var leftMaterials = $('.materials-container .materials-content .slider-left');
-  var rightMaterials = $('.materials-container .materials-content .slider-right');
-
-  var leftWork = $('.o-works-container .o-works-content .slider-left');
-  var rightWork = $('.o-works-container .o-works-content .slider-right');
-
-  if (leftMaterials || rightMaterials) {
-    slide = $('.material-single');
-    slideTotal = slide.length - 1;
-    slideCurrent = -1;
-    leftMaterials.on('click', function() {
-      slideLeft();
-    });
-    rightMaterials.on('click', function() {
-      slideRight(slide, slideCurrent, slideTotal);
-    });
+  var objMaterial = {
+    slide: $('.material-single'),
+    current: 0,
+    total: $('.material-single').length - 1
   }
 
-  if (leftWork || rightWork) {
-    slide = $('.material-single');
-    slideTotal = slide.length - 1;
-    slideCurrent = -1;
-    leftWork.on('click', function() {
-      slideLeft();
-    });
-    rightWork.on('click', function() {
-      slideRight(slide, slideCurrent, slideTotal);
-    });
+  var objWork = {
+    slide: $('.o-work-single'),
+    current: 0,
+    total: $('.o-work-single').length - 1
   }
 
+  $('.materials-container .materials-content .slider-left').on('click', function() {
+    slideLeft(objMaterial);
+  });
+  $('.materials-container .materials-content .slider-right').on('click', function() {
+    slideRight(objMaterial);
+  });
+
+  $('.o-works-container .o-works-content .slider-left').on('click', function() {
+    slideLeft(objWork);
+  });
+  $('.o-works-container .o-works-content .slider-right').on('click', function() {
+    slideRight(objWork);
+  });
 
   slideInitial();
 });
