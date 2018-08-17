1ec9919b9b96282fa762e99dcd7a2c5fbf5d8dec
Denis Sedlenko <v-dealek@microsoft.com>
2018-08-17 14:39:43 +0300

    redesign materials and minor changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,139 +1,64 @@
 $(window).load(transferInputModule);
 $(window).resize(transferInputModule); 
-$(window).on('load resize', transferInputModule);
-$(document).on('ready', function() {
-  $('.bck-vd-img').css('height', $('.bottom-header').outerHeight());
-
-  var mi = 0;
-  $(function() {
-    $('.material-single-image').lazy({
-      visibleOnly: true,
-      effect: "show",
-      effectTime: 1,
-      threshold: 0,
-      afterLoad: function(element) {
-          mi === 13 ? $('.material-single').css('background-image', 'none') : mi++;
-      }
-    });
-  });
-
-  var groups = Array.prototype.slice.call(document.querySelectorAll('.quote-btn-dots'));
-
-  var _iteratorNormalCompletion2 = true;
-  var _didIteratorError2 = false;
-  var _iteratorError2 = undefined;
-
-  try {
-    for (var _iterator2 = groups[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-      var group = _step2.value;
-      new BtnGroup(group)
-    }
-  } catch (err) {
-    _didIteratorError2 = true;
-    _iteratorError2 = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion2 && _iterator2.return) {
-        _iterator2.return();
-      }
-    } finally {
-      if (_didIteratorError2) {
-        throw _iteratorError2;
-      }
-    }
-  }
-
-  function slideInitial() {
-    $('.material-single').addClass('proactivede');
-    setTimeout(function() {
-      slideRight({ slide:$('.material-single'), current: -1, total: $('.material-single').length-1 });
-    }, 500);
-  }
-
-  function slideRight(obj) {
-    if (obj.current < obj.total) {
-      obj.current++;
-    } else {
-      obj.current = 0;
-    }
-
-    if (obj.current > 0) {
-      var preactiveSlide = obj.slide.eq(obj.current - 1);
-    } else {
-      var preactiveSlide = obj.slide.eq(obj.total);
-    }
-    var activeSlide = obj.slide.eq(obj.current);
-    if (obj.current < obj.total) {
-      var proactiveSlide = obj.slide.eq(obj.current + 1);
-    } else {
-      var proactiveSlide = obj.slide.eq(0);
-
-    }
-
-    obj.slide.each(function() {
-      var thisSlide = $(this);
-      if (thisSlide.hasClass('preactivede')) {
-        thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
-      }
-      if (thisSlide.hasClass('preactive')) {
-        thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
-      }
-    });
-    preactiveSlide.removeClass('preactivede active proactive proactivede').addClass('preactive');
-    activeSlide.removeClass('preactivede preactive proactive proactivede').addClass('active');
-    proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
-  }
-
-  function slideLeft(obj) {
-    if (obj.current > 0) {
-      obj.current--;
-    } else {
-      obj.current = obj.total;
-    }
-
-    if (obj.current < obj.total) {
-      var proactiveSlide = obj.slide.eq(obj.current + 1);
-    } else {
-      var proactiveSlide = obj.slide.eq(0);
-    }
-    var activeSlide = obj.slide.eq(obj.current);
-    if (obj.current > 0) {
-      var preactiveSlide = obj.slide.eq(obj.current - 1);
-    } else {
-      var preactiveSlide = obj.slide.eq(obj.total);
-    }
-    obj.slide.each(function() {
-      var thisSlide = $(this);
-      if (thisSlide.hasClass('proactivede')) {
-        thisSlide.removeClass('preactive active proactive proactivede').addClass('preactivede');
-      }
-      if (thisSlide.hasClass('proactive')) {
-        thisSlide.removeClass('preactivede preactive active proactive').addClass('proactivede');
-      }
-    });
-    preactiveSlide.removeClass('preactivede active proactive proactivede').addClass('preactive');
-    activeSlide.removeClass('preactivede preactive proactive proactivede').addClass('active');
-    proactiveSlide.removeClass('preactivede preactive active proactivede').addClass('proactive');
-  }
-
-  var objMaterial = {
-    slide: $('.material-single'),
-    current: 0,
-    total: $('.material-single').length - 1
-  }
-
-  $('.materials-container .materials-content .slider-left').on('click', function() {
-    slideLeft(objMaterial);
-  });
-  $('.materials-container .materials-content .slider-right').on('click', function() {
-    slideRight(objMaterial);
-  });
-
-  slideInitial();
-});
+$(document).on('ready', setSizeBackgroundImageFirstSection);
 
 function transferInputModule(){
   $(window).width() <= '480' ?
     $('.left-inputs:nth-child(2)').append($('.form-group-file')) :
     $('.left-inputs:nth-child(1)').append($('.form-group-file'));
 }
+
+function setSizeBackgroundImageFirstSection(){
+  $('.bck-vd-img').css('height', $('.bottom-header').outerHeight());
+}
+
+var navSliderOptions = {
+      loop: true,
+      loopAdditionalSlides: 2,
+      autoplay: {
+        delay: 6000,        
+        disableOnInteraction: false,
+      },
+      spaceBetween: 5,
+      slidesPerView: 7,
+      centeredSlides : true,
+      slideToClickedSlide: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        slideChangeTransitionStart: function() {
+          var swiper = this;
+          for (var i = 0; i < swiper.slides.length; i++) {
+            if (swiper.slides[i].className.indexOf('swiper-slide-active') > -1) {
+              re = /url\(\"([a-z\/-]+.[a-z]+)\"\)/gi;
+              var res = re.exec(swiper.slides[i].style.backgroundImage);
+              $('#background-monument').attr("xlink:href", res[1]);
+              $(`.${swiper.slides[i].id}__label`).toggle();
+            }
+          }
+        }
+      }
+    };
+
+var navSlider = new Swiper('.nav-slider', navSliderOptions);
+navSlider.controller.control = navSlider;
+
+var review = new Swiper('.quote-column', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.quote-button-next',
+    prevEl: '.quote-button-prev',
+  },
+});
+review.controller.control = review;
